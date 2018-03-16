d460c5aa2eabc263d77d6e7ef06fc139949bc0f9
Ben Boyle <benjamins.boyle@gmail.com>
2018-03-16 17:26:33 +1000

    add frame seeking
    - wire up forms to seek actions
    - add +1/-1 buttons for seeking by frames
    - display time/frame/fps in a table
    - add reset button for resetting all inputs

--- video-seek.js
+++ video-seek.js
@@ -1,7 +1,9 @@
 const videoSelector = document.querySelector('#select__video');
 const videoElement = document.querySelector('video');
 const outputTime = document.querySelector('#output__currentTime');
+const outputFrame = document.querySelector('#output__currentFrame');
 
+var frameRate = 24;
 
 function showVideoJson(json) {
 	document.querySelector('#text__metadata').value = json;
@@ -12,6 +14,23 @@ function selectVideo() {
 	const selectedVideoId = videoSelector.value;
 	videoElement.src = `src/${selectedVideoId}.webm`;
 
+	switch (selectedVideoId) {
+	case '6aiu25mm':
+		frameRate = 30;
+		break;
+	case '7m3nrtj3':
+		frameRate = 60;
+		break;
+	case 'uqmg2ec3':
+		frameRate = 12;
+		break;
+	default:
+		frameRate = 24;
+	}
+
+	document.querySelector('#output__frameRate').value = frameRate;
+	document.querySelector('#output__frameDuration').value = (1 / frameRate).toFixed(6);
+
 	fetch(`src/${selectedVideoId}.webm_mse_manifest.json`)
 		.then(response => response.text())
 		.then(showVideoJson);
@@ -19,19 +38,90 @@ function selectVideo() {
 
 
 function showCurrentTime() {
-	outputTime.textContent = `${videoElement.currentTime.toFixed(6)} seconds`;
+	outputTime.value = videoElement.currentTime;
+	outputFrame.value = (videoElement.currentTime * frameRate) + 1;
+}
+
+
+function stopOnSubmit(event) {
+	event.preventDefault();
+	videoElement.pause();
+}
+
+
+function gotoTime(time) {
+	videoElement.currentTime = time;
 }
 
 
 function seekTime(event) {
-	event.preventDefault();
-	videoElement.pause();
-	videoElement.currentTime = document.querySelector('#input__time').valueAsNumber;
+	stopOnSubmit(event);
+	gotoTime(document.querySelector('#input__time').valueAsNumber);
 }
 
 
+function gotoFrame(frame, offset) {
+	frame = frame - 1;
+	gotoTime((frame / frameRate) + offset);
+}
 
+
+function seekFrame(event) {
+	stopOnSubmit(event);
+	gotoFrame(document.querySelector('#input__frame').valueAsNumber, 0);
+}
+
+
+function seekFrameTenth(event) {
+	stopOnSubmit(event);
+	const offset = (1 / frameRate) / 10;
+	gotoFrame(document.querySelector('#input__frame--tenth').valueAsNumber, offset);
+}
+
+
+function seekFrameHalf(event) {
+	stopOnSubmit(event);
+	const offset = (1 / frameRate) / 2;
+	gotoFrame(document.querySelector('#input__frame--half').valueAsNumber, offset);
+}
 
 
 videoSelector.addEventListener('change', selectVideo);
-videoElement.addEventListener('timeupdate', showCurrentTime);
+
+
+// use RAF to sync frame
+function syncVideoPosition() {
+	showCurrentTime();
+	window.requestAnimationFrame(syncVideoPosition);
+}
+window.requestAnimationFrame(syncVideoPosition);
+
+
+
+function stepFrame(event) {
+	if (! event.target.matches('button[type="button"]')) {
+		return;
+	}
+
+	const step = /\+1/.test(event.target.textContent) ? 1 : -1;
+	const input = event.target.form.querySelector('input');
+
+	input.value = input.valueAsNumber + step;
+
+	// click first button in form to trigger submit event
+	event.target.form.querySelector('button').click();
+}
+
+
+document.addEventListener('click', stepFrame);
+
+
+function reset() {
+	videoElement.pause();
+	videoElement.currentTime = 0;
+
+	document.querySelector('#input__time').value = 0;
+	document.querySelector('#input__frame').value = 1;
+	document.querySelector('#input__frame--tenth').value = 1;
+	document.querySelector('#input__frame--half').value = 1;
+}
